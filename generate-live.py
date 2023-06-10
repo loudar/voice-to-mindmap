8bee37ab53d10fb9c8b8f912b134bdf6a90dca12
alexa <alex@trirecords.eu>
2023-06-10 15:08:16 +0200

    start with dash

--- generate-live.py
+++ generate-live.py
@@ -6,8 +6,9 @@ import keyboard
 from datetime import datetime
 
 from lib.mapper import create_mind_map_force
+from lib.plotly_wrapper import create_plot
 from lib.text_processing import extract_logical_links
-from plotter import create_plot
+from dash import Dash
 
 
 def voice_to_text(q, stop_event_ref):
@@ -17,7 +18,8 @@ def voice_to_text(q, stop_event_ref):
             text = r.recognize_google(audio_source, language=google_lang[selected_lang])
             q.put(text)  # Put the recognized text in the queue
             print(f"Recognized Text: {text}")
-            update_plot(q)
+            update_thread = threading.Thread(target=update_plot, args=(q,))
+            update_thread.start()
         except sr.UnknownValueError:
             print("Speech recognition could not understand audio.")
         except sr.RequestError as e:
@@ -45,7 +47,8 @@ def voice_to_text(q, stop_event_ref):
 
 
 def update_plot(q):
-    global accumulated_text
+    global accumulated_text, update
+
     # Check if new text is available in the queue
     while not q.empty():
         text = q.get()  # Get the latest recognized text
@@ -59,33 +62,8 @@ def update_plot(q):
 
     # Create the mind map using the accumulated logical links
     G, subgraph_positions = create_mind_map_force(logical_links)
-    create_plot(G, subgraph_positions)
-
-
-languages = ['en', 'de']
-google_lang = {
-    'en': 'en-US',
-    'de': 'de-DE'
-}
-google_lang_arr = ['en-US', 'de-DE']
-
-selected_lang = 'de'
-
-r = sr.Recognizer()
-accumulated_text = ""
-
-# use current timestamp as unique identifier for the transcript
-conversation_id = datetime.now().strftime("%Y-%m-%d-%H_%M_%S")
-transcript_file = f"transcripts/transcript_{conversation_id}.txt"
-
-# Create a queue to store the recognized text
-text_queue = queue.Queue()
-
-# Create an event to control the audio recording
-stop_event = threading.Event()
-
-# Create a lock to synchronize access to the accumulated text
-lock = threading.Lock()
+    create_plot(G, subgraph_positions, update)
+    update = True
 
 
 # Function to handle the stop key press
@@ -114,11 +92,48 @@ keyboard.on_press_key("F2", stop_key_press)
 keyboard.on_press_key("F4", handle_lang_change)
 keyboard.on_press_key("F3", handle_reset)
 
+languages = ['en', 'de']
+google_lang = {
+    'en': 'en-US',
+    'de': 'de-DE'
+}
+google_lang_arr = ['en-US', 'de-DE']
+
+selected_lang = 'de'
+print(f"Selected language: {selected_lang}")
+
+r = sr.Recognizer()
+accumulated_text = ""
+global update
+update = False
+
+# use current timestamp as unique identifier for the transcript
+conversation_id = datetime.now().strftime("%Y-%m-%d-%H_%M_%S")
+transcript_file = f"transcripts/transcript_{conversation_id}.txt"
+
+# Create a queue to store the recognized text
+text_queue = queue.Queue()
+
+# Create an event to control the audio recording
+stop_event = threading.Event()
+
+# Create a lock to synchronize access to the accumulated text
+lock = threading.Lock()
+
+# check if the directory exists. If not, create it
+if not os.path.exists('transcripts'):
+    os.makedirs('transcripts')
+
 # Create a thread for continuous audio recording
 audio_thread = threading.Thread(target=voice_to_text, args=(text_queue, stop_event))
 print("Starting audio thread...")
 audio_thread.start()
 
-# check if the directory exists. If not, create it
-if not os.path.exists('transcripts'):
-    os.makedirs('transcripts')
+app = Dash(__name__)
+if __name__ == '__main__':
+    app.run(debug=True)
+
+while not stop_event.is_set():
+    pass
+
+audio_thread.join()
---
+++ lib/plotly_wrapper.py
@@ -0,0 +1,74 @@
+import plotly.graph_objects as go
+from lib.colors import generate_unique_color
+
+def create_plot(G, subgraph_positions, update=False):
+    print("Creating plot...")
+    # Create Plotly figure
+    edge_traces = []
+
+    category_colors = {}  # Dictionary to store unique colors per category
+
+    max_degree = max(dict(G.degree()).values())  # Calculate the maximum degree in the graph
+
+    for edge in G.edges(data=True):
+        x0, y0 = subgraph_positions[edge[0]]
+        x1, y1 = subgraph_positions[edge[1]]
+        weight = edge[2]['weight'] if 'weight' in edge[2] else 1
+
+        edge_trace = go.Scatter(
+            x=[x0, (x0 + x1) / 2, x1, None],
+            y=[y0, (y0 + y1) / 2, y1, None],
+            line=dict(width=weight, color='#888'),
+            hoverinfo='text',
+            hovertext=f"{edge[0]} -> {edge[1]} ({weight})",
+            mode='lines',
+            showlegend=False,
+        )
+
+        edge_traces.append(edge_trace)
+
+    for node in G.nodes():
+        category = G.nodes[node].get('category', 'default')  # Get the category property
+        size = G.degree[node] / max_degree  # Calculate relative node size based on the degree
+
+        if category not in category_colors:
+            # Generate a unique color for the category with good contrast against white
+            color = generate_unique_color(category_colors.values())
+            category_colors[category] = color
+        else:
+            color = category_colors[category]
+
+        node_trace = go.Scatter(
+            x=[subgraph_positions[node][0]],
+            y=[subgraph_positions[node][1]],
+            mode='markers',
+            hoverinfo='text',
+            marker=dict(
+                showscale=False,
+                color=f"rgb{color}",
+                size=size * 50,  # Adjust the scaling factor as per your preference
+                line=dict(width=2)
+            ),
+            text=[node + f" ({category})"],
+        )
+
+        edge_traces.append(node_trace)
+
+    go_fig = go.Figure(data=edge_traces,
+                       layout=go.Layout(
+                           title='Generated Mind Map',
+                           titlefont=dict(size=16),
+                           showlegend=False,
+                           hovermode='closest',
+                           margin=dict(b=0, l=0, r=0, t=40),
+                           annotations=[dict(
+                               xref="paper", yref="paper",
+                               x=0.005, y=-0.002)],
+                           xaxis=dict(showgrid=False, zeroline=False, showticklabels=False),
+                           yaxis=dict(showgrid=False, zeroline=False, showticklabels=False))
+                       )
+
+    if update:
+        go_fig.update()
+    else:
+        go_fig.show()
